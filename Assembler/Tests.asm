ed72d807c3d16f794aeb0ec4bfdbced468d90e25
Alex Hirschfeld <alex@d-qoi.com>
2016-01-22 22:18:11 -0500

    Tests cases for things.

---
+++ Assembler/Tests.asm
@@ -0,0 +1,46 @@
+# testing branching down more than 16
+test:
+beq $t0 $0 test3
+# should become
+# 0x3801
+# 0x6310
+
+
+r $t1 0($t1) 
+#should become 0x7990
+
+add $t1 $t2 15 
+#should become 
+# 0x19A9
+# 0xF
+
+beq $h1 $t0 test2
+# should become 
+# 0x2d80
+
+test2:
+jr $pc test
+# should become 
+# 0x63fc
+
+#expanding for testing
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+and $0 $0
+test3:
+slt $0 $0
+
+#now testing branch up
+beq $0 $t1 test3
+
+# don't know what this does
+jal test3
