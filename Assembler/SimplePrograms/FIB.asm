ffbaf4e949d2ce52ddaa99dcf658760457268d62
Alex Hirschfeld <alex@d-qoi.com>
2016-01-25 00:57:40 -0500

    Fixing things

--- Assembler/SimplePrograms/FIB.asm
+++ Assembler/SimplePrograms/FIB.asm
@@ -4,19 +4,19 @@ main:
 	cpy $t0 0
 	cpy $t1 1
 
-	cpy $s0 $t0 #setting up for alt path through loop
+	cpy $s0 $0 #setting up for alt path through loop
 	cpy $s1 500 #ending number
 
 	rsh 12 #for sudo call
 
 start:
 	beq $s0 $0 two
-	add $t1 $t0
 	cpy $h0 $t1
+	add $t1 $t0
 	j end
 two:
-	add $t0 $t1
 	cpy $h0 $t0
+	add $t0 $t1
 end:
 	not $s0 $0
 	sudo 10
